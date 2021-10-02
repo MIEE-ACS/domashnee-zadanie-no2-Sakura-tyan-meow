80ffaa70a267a15ee7cce47aef1b35066bef9ec3
Sakura-tyan-meow <90412520+Sakura-tyan-meow@users.noreply.github.com>
2021-10-02 13:31:16 +0300

    vot tak vot

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,49 +9,65 @@ namespace dz2
 
             double y;
             //y = double.Parse(Console.ReadLine());
-            for (double x = -9; x <= 5; x++)
+            double R = double.Parse(Console.ReadLine());
+            for (double x = -9; x <= 5; x = x+0.01)
             {
-                if (x < -5)
-                {
-
-
-                    int R = 4;
-                    y = Math.Sqrt(R - x * x);
-                    Console.WriteLine(y);
-
-                }
-                else if (x >= -5 && x <= -4)
-                {
-                    y = 2;
-                    Console.WriteLine(y);
-                }
-                else if (x > -4 && x <= 0)
-                {
-                    y = -x;
-                    Console.WriteLine(y);
-                }
-                else if (x >= 0 && x <= Math.PI)
-                {
-                    y = Math.Sin(x);
-                    Console.WriteLine(y);
-                }
-                else if (x >= Math.PI)
-                {
-                    y = x;
-                    Console.WriteLine(y);
-                }
+                @if(R, x);
+                
             }
-
+            while (true)
+            {
+                Console.WriteLine("введите x :");
+               double x = double.Parse(Console.ReadLine());
+                 @if(R, x);
+                
+            }
 
 
         }
 
+        static void @if(double R, double x)
+        {
+            double y = 0;
+            if (x < -5)
+            {
+
+                //int R = Console.ReadLine();
 
 
+                if (R < 4)
+                {
+                    Console.WriteLine("функция не определена на данном участке");
+                }
+                else
+                {
+                    y = Math.Round(Math.Sqrt(R - x * x), 5);
+                    Console.WriteLine($"{y}");
+                }
 
+            }
+            else if (x >= -5 && x <= -4)
+            {
+                y = 2;
+                Console.WriteLine($"{y}");
+            }
+            else if (x > -4 && x <= 0)
+            {
+                y = Math.Round(-x, 5);
+                Console.WriteLine($"{y}");
+            }
+            else if (x >= 0 && x <= Math.PI)
+            {
+                y = Math.Round(Math.Sin(x), 5);
+                Console.WriteLine($"{y}");
+            }
+            else if (x >= Math.PI)
+            {
+                y = Math.Round(x, 5);
+                Console.WriteLine($"{y}");
+            }
 
-
-
-
+           
+        }
     }
 }
